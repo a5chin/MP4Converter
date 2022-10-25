fb2bb710d0a7b36b3381f4a9c2cef4dbfe81ec47
a5chin <capricorn42551230@gmail.com>
2022-10-26 04:42:44 +0900

    modified opencv to moviepy

--- lib/__init__.py
+++
@@ -1,1 +0,0 @@
-from lib.converter import MP4Converter
--- main.py
+++ main.py
@@ -1,9 +1,48 @@
-from lib import MP4Converter
+import argparse
+from pathlib import Path
+
+from moviepy.editor import VideoFileClip
+
+
+def make_parse() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--inputs",
+        default="inputs",
+        type=str,
+        help="plese set input file path",
+    )
+    parser.add_argument(
+        "--outputs",
+        default="outputs",
+        type=str,
+        help="plese set gray image path",
+    )
+    parser.add_argument(
+        "--ext",
+        default="mp4",
+        type=str,
+        help="plese set extension",
+    )
+
+    return parser.parse_args()
 
 
 def main():
-    converter = MP4Converter()
-    converter.convert(filename='ocean.mts', output='output.mp4')
+    args = make_parse()
+
+    inputs = Path(args.inputs)
+    input_list = [filename for filename in inputs.glob("**/*")]
+
+    for filename in input_list:
+        video = VideoFileClip(filename.as_posix())
+        video.write_videofile(
+            f"outputs/{filename.stem}.{args.ext}",
+            codec='libx264',
+            audio_codec='aac',
+            remove_temp=True
+        )
 
 
 if __name__ == '__main__':
